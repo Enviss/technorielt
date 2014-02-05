98a058142346aafe2b5f5bbedeeaee5969925398
Evgeny Bogomolov <bogomolov.evgeniy@gmail.com>
2014-02-05 19:16:13 +0400

    Первый шаг

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Technorielt::Application.config.secret_key_base = '21224c97a7f73def5e81328905300b8146a361e9c8473114d9904fbde7e9c561a4245e67dcba55c94f9bf02dbe22c922b57573d1fbbfc449b7e4bc8bc1080afd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Technorielt::Application.config.session_store :cookie_store, key: '_technorielt_session'
